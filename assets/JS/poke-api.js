417940dbd7db2015b0143aa64fe6bdd4fc9bc456
Henrique Mateus Vargas dos Santos <108635477+DevHenriquecido@users.noreply.github.com>
2023-05-28 10:16:45 -0300

    Update poke-api.js

--- assets/JS/poke-api.js
+++ assets/JS/poke-api.js
@@ -1,7 +1,11 @@
+//Cria objetos a serem pesquisados
 const pokeApi = {}
 
+//Função que declara cada informção do pokemon em um objeto
 function convertPokeApiDetailToPokemon(pokeDetail) {
+    //Cria sua classe
     const pokemon = new Pokemon()
+
     pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
 
@@ -26,18 +30,28 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     return pokemon
 }
 
+//Função que transforma as informações da API para poderem ser lidas pelo Javascript e poder
+//ser manipulada
 pokeApi.getPokemonDetail = (pokemon) => {
+    //Pega da API
     return fetch(pokemon.url)
+    //Converte para ser interpretado
         .then((response) => response.json())
         .then(convertPokeApiDetailToPokemon)
 }
+//Função que carrega todas as informações da API inclusa para carrega no Browser
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
+    //Pega o endereço
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
    return fetch(url)
+   //Converte para ser interpretado
     .then((response) =>  response.json())
     .then((jsonBody) => jsonBody.results)
+    //Mapeia tudo
     .then((pokemons => pokemons.map(pokeApi.getPokemonDetail)))
+    //Faz o browser carregar o solicitado
     .then((detailRequests) => Promise.all(detailRequests))
+    //Envia para a maniupulaçãop
     .then((pokemonsDetails) => pokemonsDetails)
 }
 
