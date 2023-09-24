feaeee7b661c82e958ace5551b653b5dae03d2dc
unknown <henriquem.vdossantos@gmail.com>
2023-09-24 17:57:32 -0300

    feat: query API with axios

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,9 +1,20 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+import { useState } from 'react'
 
-function Home (){
+function Home() {
+
+    const [pokemon, setPokemon] = useState()
+
+    axios.get(`https://pokeapi.co/api/v2/pokemon/1`)
+        .then((response) => setPokemon(response.data))
+
     return (
         <div>
             <h1>Pokedex - Primeira Geração</h1>
+            <ul>
+                <li>{pokemon?.name}</li>
+            </ul>
         </div>
     )
 }
