14f3b5de07029cb8c5a6b619e27bc826ae7793c0
DevHenriquecido <henriquem.vdossantos@gmail.com>
2023-10-09 22:22:22 -0300

    feat: Filter pokemons by types

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -8,6 +8,7 @@ function Home() {
     
    
 const [pokemons, setPokemons] = useState([])
+const [pokemonsByType, setPokemonsByType] = useState([])
 const [name, setName] = useState('')
 const api = 'https://pokeapi.co/api/v2/pokemon'
 const limitList = 151
@@ -55,7 +56,31 @@ const searchPokemon = () => {
       setName(target.value)
   }
 
+const filterTypes = () => {
+
+let select = document.getElementById('StatusType')
+let value = select.options[select.selectedIndex].value
+
+  if (value === 'all'){
+    setPokemonsByType([])
+    getPokemons(offsetList, limitList) 
+  }
+
+pokemons.map((pokemon, i) => {
+    pokemon.types.map((typeSlot) => {
+    if (typeSlot.type.name == value) {
+        setPokemons([])
+        setPokemonsByType((prev) => {{
+            return [...prev, pokemon]
+        }})
+    }
+    })
+    }
+)
+}
+
 console.log(pokemons)
+console.log(pokemonsByType)
 
 useEffect(() => {  
  if (pokemons.length < limitList){
@@ -74,8 +99,45 @@ useEffect(() => {
         value={name}
       />
       <button onClick={searchPokemon}>Buscar</button>
+
+<select id='StatusType'>
+<option value='all'>All</option>
+<option value='normal'>Normal</option>
+<option value='grass'>Grass</option>
+<option value='fire'>Fire</option>
+<option value='water'>Water</option>
+<option value='electric'>Electric</option>
+<option value='ice'>Ice</option>
+<option value='ground'>Ground</option>
+<option value='flying'>Flying</option>
+<option value='poison'>Poison</option>
+<option value='fighting'>Fighting</option>
+<option value='psychic'>Psychic</option>
+<option value='dark'>Dark</option>
+
+      </select>
+
+      <button onClick={filterTypes}>Filtrar</button>
             <ol id="pokemonList" className="pokemons"> 
             {pokemons.map((pokemon, i) => (
+             <> 
+            <Link style={{textDecoration: 'none'}} to={`/DetailsPokemons/${pokemon.id}`}>
+             <li id="pokemon" className={pokemon.types.map((typeSlot) =>  typeSlot.type.name).splice(0, 1)}> 
+            <span className="name">{pokemon.name}</span>
+            <span className="number">#{pokemon.id}</span>
+            <div className="detail">
+            <ol className="types">
+             {pokemon.types.map((typeSlot) => <li  id='type' className={typeSlot.type.name}>{typeSlot.type.name}</li>)}
+             </ol>
+             <img src={pokemon.sprites.other.dream_world.front_default}/>
+             </div>
+             </li>
+             </Link>
+             
+             </>
+             ))}
+
+        {pokemonsByType.map((pokemon, i) => (
              <> 
             <Link style={{textDecoration: 'none'}} to={`/DetailsPokemons/${pokemon.id}`}>
              <li id="pokemon" className={pokemon.types.map((typeSlot) =>  typeSlot.type.name).splice(0, 1)}> 
